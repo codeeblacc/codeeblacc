bac8654280b0920f60d37464edb7d39aafe3953c
codeeblacc <dycode999@gmail.com>
2022-08-22 18:07:59 +0100

    Portfolio works update

--- src/utils.js
+++ src/utils.js
@@ -13,30 +13,27 @@ export function getDate(date) {
 export const studies = [
   {
     "id": "1",
-    "slug": "guitar-tuner-plus",
-    "name": "Guitar tuner+",
-    "description": "A leading app in guitar tuning technology. Guitar tuner+ is an app that gives you fast sonic responses, clear and visible tuning instruction for your guitar.",
-    "category": "Brand Design",
+    "slug": "oshify",
+    "name": "OshiFy",
+    "description": "This is a visual identity to act as a foundation for future asset development and give Oshify the solid, hazard inspired aesthetic needed.",
+    "category": "Brand Visual Identity",
     "year": "2021",
     "content": {
       0: {
-        "title": "Create Something out of Nothing",
-        "description": "In the beginning, it was crucial for us to understand what musicians expect from a tuner app. For an outsider, it might seem like a somewhat serious guitarist would not use a conventional app but a hardware tuner of some sort. In reality, professionals use apps they can find on the App Store(and you'd be suprised by some of their choices, Honeyhoney on JRE), or tune by ear from the notes they can get from GarangeBand(whadup, Ola Engulnd).",
+        "title": "Challenge and Role",
+        "description": "Oshify is a SAAS Tech company developing simple safety software for high-hazard industries. My role in this project was to create different variations of the base design. I took this as a challange to explore 3D computer software and thier capacity to create realistic materials."
       },
       1: {
         "title": "Production technology in detail",
-        "description": "We proceeded with the app design used brands colors, making it utilitarian, sleek, and elegant. In terms of development, we built the app using Swift Programming language which is the primary language for iOS app development today.",
+        "description": "We proceeded with the app design used brands colors, making it utilitarian, sleek, and elegant. In terms of development, we made the design using Adobe Illustrator and After Effects which is the primary software for 3D design today.",
       },
     },
     "assets" : {
-      0: "/images/cstud-01-01.jpg",
-      1: "/images/cstud-01-02.jpg",
-      2: "/images/cstud-01-03.jpg",
-      3: "/images/cstud-01-04.jpg",
-      4: "/images/cstud-01-05.jpg",
-      5: "/images/cstud-01-06.jpg",
-      6: "/images/cstud-01-07.jpg",
-      7: "/images/cstud-01-08.jpg",
+      0: "https://mir-s3-cdn-cf.behance.net/project_modules/2800_opt_1/beff18144613521.628f5bdea0de1.png",
+      1: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/d6b1c2144613521.62920fff73310.png",
+      2: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/95ce5c144613521.6291309ab451b.png",
+      3: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/74fd43144613521.628f5797dcd98.png",
+      4: "https://mir-s3-cdn-cf.behance.net/project_modules/fs/105087144613521.62921700231c2.png",
     }
   }
 ]
